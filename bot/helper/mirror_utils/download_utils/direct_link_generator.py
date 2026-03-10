3c79a8f84e3d493ad79641492075a221ed6212da
Rumal Gunawardhana <42924158+rumalg123@users.noreply.github.com>
2026-03-10 17:09:48 +0530

    Update direct_link_generator.py
    Signed-off-by: Rumal Gunawardhana <42924158+rumalg123@users.noreply.github.com>

--- bot/helper/mirror_utils/download_utils/direct_link_generator.py
+++ bot/helper/mirror_utils/download_utils/direct_link_generator.py
@@ -5,7 +5,7 @@ from json import loads
 from os import path
 from uuid import uuid4
 from hashlib import sha256
-from time import sleep
+from time import sleep, time
 from re import findall, match, search
 from secrets import token_hex
 from requests.adapters import HTTPAdapter
@@ -1532,13 +1532,17 @@ def gofile(url, auth):
 
     def __fetch_links(session, _id, folderPath=""):
         _url = f"https://api.gofile.io/contents/{_id}?cache=true"
+        time_slot = int(time()) // 14400
+        raw = f"{user_agent}::en-US::{token}::{time_slot}::gf2026x"
+        wt = sha256(raw.encode()).hexdigest()
         headers = {
             "User-Agent": user_agent,
             "Accept-Encoding": "gzip, deflate, br",
             "Accept": "*/*",
             "Connection": "keep-alive",
             "Authorization": "Bearer" + " " + token,
-            "X-Website-Token" : "4fd6sg89d7s6",
+            "X-Website-Token": wt,
+            "X-BL": "en-US"
         }
         if _password:
             _url += f"&password={_password}"
