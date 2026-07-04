4d903562a503a30527841ce40685f301108b386c
Thomas Hacks <93702860+pachax001@users.noreply.github.com>
2026-07-04 20:50:22 +0530

    Update direct_link_generator.py
    Signed-off-by: Thomas Hacks <93702860+pachax001@users.noreply.github.com>

--- bot/helper/mirror_utils/download_utils/direct_link_generator.py
+++ bot/helper/mirror_utils/download_utils/direct_link_generator.py
@@ -1533,7 +1533,7 @@ def gofile(url, auth):
     def __fetch_links(session, _id, folderPath=""):
         _url = f"https://api.gofile.io/contents/{_id}?cache=true"
         time_slot = int(time()) // 14400
-        raw = f"{user_agent}::en-US::{token}::{time_slot}::5d4f7g8sd45fsd"
+        raw = f"{user_agent}::en-US::{token}::{time_slot}::9844d94d963d30"
         wt = sha256(raw.encode()).hexdigest()
         headers = {
             "User-Agent": user_agent,
